7c543743f81f2aadbe6640cef81b89c20db40c8f
samanthacastrorajbe <samanthacastrorajbe@gmail.com>
2025-03-17 15:58:17 -0400

    REGISTRARSE FUNCIONA (falta guardar foto de perfil)

--- src/js/supabaseConfig.js
+++ src/js/supabaseConfig.js
@@ -5,6 +5,33 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const uploadImage = async (file, bucket, folder) => {
+    try {
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${Math.random()
+        .toString(36)
+        .substring(2, 15)}_${Date.now()}.${fileExt}`;
+  
+      const filePath = `${folder}/${fileName}`;
+  
+      const { error } = await supabase.storage
+        .from(bucket)
+        .upload(filePath, file, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+  
+      const { data: urlData } = supabase.storage
+        .from(bucket)
+        .getPublicUrl(filePath);
+  
+      return urlData.publicUrl;
+    } catch (error) {
+      console.error("Error al subir la imagen:", error);
+      throw error;
+    }
+  };
+
 
 //clave api: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuamd4Z2l4dmJqenhzanJ5ZWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNjU0OTIsImV4cCI6MjA1Nzc0MTQ5Mn0.oJ3e4CO4cHlpbnB9uoKpsK6fnvqzG2F-uhfqFabrgmI
 //url pryecto: https://enjgxgixvbjzxsjryelr.supabase.co
